230dc06c7bcb0ed38233945096eb3f564bfc6dbe
Andrea <bbooss97@gmail.com>
2022-08-10 18:31:55 +0200

    tried to train but failed

--- yolov5.py
+++ yolov5.py
@@ -4,17 +4,26 @@ import numpy as npù
 import torchvision.transforms as transforms
 import os
 import random
+import sys
+import torch.optim as optim
+sys.path.append("./yolov5/utils")
+sys.path.append("./yolov5")
+from MyLoss import ComputeLoss
 # Model
 model = torch.hub.load('./yolov5/', 'custom', path='yoloaugmentedbest.pt', source='local') 
+for params in model.parameters():
+    params.requires_grad = True
+# model.train()
 # print(model)
 # model.load_state_dict(torch.load("./best.pt"))
-model.eval()
+# model.test()
 # print(model)
 # Images
 # img=Image.open('./trainAnnotated/1B1B1N2-1r6-n2R2k1-7b-1B6-8-8-Kn6.jpeg')
 path=os.listdir("./testAnnotated")
 path=path[random.randint(0,len(path))].replace(".txt",".jpeg")
 print(path)
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 img=Image.open('./testAnnotated/'+path)
 
 fromPilToTensor = transforms.Compose([
@@ -29,8 +38,24 @@ randomPerspective = transforms.RandomPerspective(distortion_scale=.0, p=1, inter
 img_tensor = randomPerspective(img)
 
 
+# print(model)
 # Inference
+# tensore=fromPilToTensor(img_tensor).unsqueeze(dim=0)
+# print(tensore.shape)
+
 results = model(img_tensor)
+optimizer.zero_grad()
+r=results.pred[0].float()
+loss=(torch.ones_like(r)-r).sum()
+print(loss)
+loss.requires_grad=True
+loss.backward()
+optimizer.step()
+print(r.shape)
+# print(type(model.model[-1]))
+# print(model[-1])
+# model.train()
+# loss=ComputeLoss(model)
 
 # Results
 results.show()
